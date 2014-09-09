bcf164d4022a32945fbf6fcd025e135bfc676820
marcusmolchany <marcusmolchany@gmail.com>
2014-09-08 21:54:18 -0400

    moved css from jquery to css file

--- js/custom.js
+++ js/custom.js
@@ -9,16 +9,25 @@ var main = function() {
   //http://stackoverflow.com/questions/1981645/stagger-jquery-animations
   //http://jsfiddle.net/jtbowden/ykbgT/
 
+  // Previous attempts
+
   // $('.sickaf-header').fadeIn(500,'easeIn').css({opacity:0,'margin-top':'150%'}).animate({opacity:1,'margin-top':'0'});
 
   // $('.sickaf-timebomb').delay(25).fadeIn('fast',$(this)).css({opacity:0,'margin-top':'150%'}).animate({opacity:1,'margin-top':'0%'});
   // $('.sickaf-weather').delay(50).fadeIn('fast',$(this)).css({opacity:0,'margin-top':'150%'}).animate({opacity:1,'margin-top':'0%'});
 
 
-  $('.sickaf-header').fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0'});
+  // $('.sickaf-header').fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0'});
 
-  $('.sickaf-timebomb').delay(50).fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0%'});
-  $('.sickaf-weather').delay(125).fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0%'});
+  // $('.sickaf-timebomb').delay(50).fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0%'});
+  // $('.sickaf-weather').delay(125).fadeIn(100,'easeIn').css({opacity:0,'margin-top':'25%'}).animate({opacity:1,'margin-top':'0%'});
+
+  // End pervious attempts
+
+  $('.sickaf-header').animate({opacity:1,'margin-top':'0'});
+
+  $('.sickaf-timebomb').delay(50).animate({opacity:1,'margin-top':'0'});
+  $('.sickaf-weather').delay(125).animate({opacity:1,'margin-top':'0'});
 
 };
 
